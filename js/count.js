ec1eddd4d28a7df96cd5c8ca360e376388849eba
RichelieuGVG1 <uryanovvasya@gmail.com>
2020-04-11 02:20:01 +0300

    Update count.js

--- js/count.js
+++ js/count.js
@@ -10,9 +10,6 @@ function setCookie(name, value, expires, path, domain, secure) {
 		if (confirm("Cookie exceeds 4KB and will be cut!"))
 			document.cookie = curCookie
 }
-
-// name - name of the desired cookie
-// * return string containing value of specified cookie or null if cookie does not exist
 function getCookie(name) {
 	var prefix = name + "="
 	var cookieStartIndex = document.cookie.indexOf(prefix)
@@ -23,11 +20,6 @@ function getCookie(name) {
 		cookieEndIndex = document.cookie.length
 	return unescape(document.cookie.substring(cookieStartIndex + prefix.length, cookieEndIndex))
 }
-
-// name - name of the cookie
-// [path] - path of the cookie (must be same as path used to create cookie)
-// [domain] - domain of the cookie (must be same as domain used to create cookie)
-// * path and domain default if assigned null or omitted if no explicit argument proceeds
 function deleteCookie(name, path, domain) {
 	if (getCookie(name)) {
 		document.cookie = name + "=" + 
@@ -36,17 +28,12 @@ function deleteCookie(name, path, domain) {
 		"; expires=Thu, 01-Jan-70 00:00:01 GMT"
 	}
 }
-
-// date - any instance of the Date object
-// * you should hand all instances of the Date object to this function for "repairs"
-// * this function is taken from Chapter 14, "Time and Date in JavaScript", in "Learn Advanced JavaScript Programming"
 function fixDate(date) {
 	var base = new Date(0)
 	var skew = base.getTime()
 	if (skew > 0)
 		date.setTime(date.getTime() - skew)
 }
-
 var now = new Date()
 fixDate(now)
 now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000)
